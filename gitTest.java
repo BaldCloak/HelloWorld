3fc70a5b48c627e1a3434194d6524e8d1d8e91e0
adolfweismann <adolfweismann@outlook.com>
2016-07-28 22:52:03 +0800

    修改编码

--- gitTest.java
+++ gitTest.java
@@ -1,8 +1,8 @@
-public calss gitTest{
+ï»¿public calss gitTest{
 	public void main(String[] args){
 		System.out.println("Everything that has a beginning has an end");
 		System.out.println("End");
-		//ÖĞÎÄ×¢ÊÍ£ºàË~~~~
+		//ä¸­æ–‡æ³¨é‡Šï¼šå—¨~~~~
 	}
 	
 }
