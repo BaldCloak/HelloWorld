722c71db670c67218cd0d9c67366c0e795bd0a24
adolfweismann <adolfweismann@outlook.com>
2016-07-28 22:45:25 +0800

    中文注释commit

--- gitTest.java
+++ gitTest.java
@@ -2,6 +2,7 @@ public calss gitTest{
 	public void main(String[] args){
 		System.out.println("Everything that has a beginning has an end");
 		System.out.println("End");
+		//ÖĞÎÄ×¢ÊÍ£ºàË~~~~
 	}
 	
 }
